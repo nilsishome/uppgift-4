40b0d9e53cc193a567ec69842c8b042f0df1c430
nilsishome <nilsnyberg24@gmail.com>
2024-09-24 16:44:20 +0200

    Finished the JS code + some CSS-changes.

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ completedTasks.innerHTML = tasks;
 
 // Declare an array that will contain the todo-list objects.
 const listArray = [];
+let listId = 1;
 
 // Add an onclick event when button is pressed.
 button.addEventListener(
@@ -21,13 +22,27 @@ button.addEventListener(
     addItem
 );
 
+// Function that edits the completion status on the objects, in the array.
+function listStatus(text, status)
+{
+    let index = listArray.map(l => l.name).indexOf(text);
+    listArray[index].completed = status;
+};
+
+// Function that removes an object in the array.
+function removeElement(text)
+{
+    let index = listArray.map(l => l.name).indexOf(text);
+    listArray.splice(index, 1);
+};
+
 // Function that appends the text from input field to a list in HTML.
 function addItem()
 {   
     // Declare two elements that will be created in this function.
     const item = document.createElement('li');
     const check = document.createElement('span');
-    const trash = document.createElement('i');
+    const trash = document.createElement('span');
     const itemLabel = document.createElement('span');
 
     // Declare a text variable that will be contained in the span element, that was created.
@@ -35,11 +50,12 @@ function addItem()
     itemLabel.innerText = text;
 
     // Checkbox sign.
-    check.setAttribute('id', 'check');
+    check.setAttribute('class', 'check');
     check.innerHTML = '☑';
 
-    // Sets an imported class attribute for the trash can.
-    trash.setAttribute('class', 'fa fa-trash-o');
+    // Sets a class attribute for the trash can.
+    trash.setAttribute('class', 'trashcan');
+    trash.innerHTML = '&#128465;';
 
     // An if statement that makes sure the input field is NOT empty.
     if (text.length == 0)
@@ -49,55 +65,63 @@ function addItem()
     } 
     else
     {   
-        // Append the list- and span-element to the DOM.
-        list.appendChild(item);
-        item.appendChild(itemLabel);
-        item.appendChild(check);
-        item.appendChild(trash);
-
-        // Pushes the span objects into the declared array.
-        listArray.push(itemLabel);
-
-        // A click event that marks the ToDo tasks: complete.
-        check.addEventListener(
-            'click',
-            function() 
-            { 
-                if (itemLabel.getAttribute('class') == 'completed')
-                {
-                    completedTasks.innerHTML = --tasks;
-                    itemLabel.setAttribute('class', '');
-                }
-                else
-                {
-                    completedTasks.innerHTML = ++tasks;
-                    itemLabel.setAttribute('class', 'completed');
-                }
-            }
-        );
-
-        // A click event that removes a ToDo task completely.
-        trash.addEventListener(
-            'click',
-            function() 
-            { 
-                if (itemLabel.getAttribute('class') == 'completed')
-                {
-                    completedTasks.innerHTML = --tasks;
-                    listArray.pop();
-                    item.remove();
-                }
-                else
-                {
-                    listArray.pop();
-                    item.remove();
-                }
-            }
-        );
-
         // Empties the input field and the error message, of all text.
         document.querySelector('#error').innerText = '';
         input.value = '';
-        return;
     }
+
+    // Append the list- and span-element to the DOM.
+    list.appendChild(item);
+    item.appendChild(itemLabel);
+    item.appendChild(check);
+    item.appendChild(trash);
+
+    // Pushes the lists as objects into the declared array.
+    let status = false;
+    const listObjects = {id: listId, name: text, completed: status};
+    listArray.push(listObjects);
+    listId++;
+
+    // A click event that marks the ToDo tasks: complete.
+    check.addEventListener(
+        'click',
+        function() 
+        { 
+            if (itemLabel.getAttribute('class') == 'completed')
+            {
+                completedTasks.innerHTML = --tasks;
+                itemLabel.setAttribute('class', '');
+                let listText = item.firstChild.textContent;
+                listStatus(listText, false);
+            }
+            else
+            {
+                completedTasks.innerHTML = ++tasks;
+                itemLabel.setAttribute('class', 'completed');
+                let listText = item.firstChild.textContent;
+                listStatus(listText, true);
+            }
+        }
+    );
+
+    // A click event that removes a ToDo task completely.
+    trash.addEventListener(
+        'click',
+        function() 
+        { 
+            if (itemLabel.getAttribute('class') == 'completed')
+            {
+                completedTasks.innerHTML = --tasks;
+                let listText = item.firstChild.textContent;
+                removeElement(listText);
+                item.remove();
+            }
+            else
+            {
+                let listText = item.firstChild.textContent;
+                removeElement(listText);
+                item.remove();
+            }
+        }
+    );
 };
